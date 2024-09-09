b80644ae6cc104e61b3c3d2fcf24391f42514edd
Litchi Pi <litchi.pi@proton.me>
2024-09-09 11:43:30 +0200

    Work to a usable chatbot
    Signed-off-by: Litchi Pi <litchi.pi@proton.me>

--- chat.py
+++ chat.py
@@ -1,1 +1,81 @@
-import gpt4all
+import sys
+import time
+import os
+from gpt4all import GPT4All
+
+MODEL_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), ".model")
+CHAT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), ".chat")
+os.makedirs(MODEL_DIR, exist_ok=True)
+os.makedirs(CHAT_DIR, exist_ok=True)
+
+FILENAME_GENERATION_PROMPT = """
+Create a very short filename for this conversation, using key words, output in JSON using the key \"conversation_filename\", only output JSON data.
+"""
+
+SYSTEM_PROMPT = """
+"""
+
+model = GPT4All(
+    model_name = "Meta-Llama-3-8B-Instruct.Q4_0.gguf",
+    model_path = MODEL_DIR,
+    allow_download = True,
+    n_threads = 16,
+)
+
+
+with model.chat_session():
+    while True:
+        try:
+            question = input("> ")
+        except:
+            print("Stopping")
+            print("")
+            break
+
+        if question == "/quit":
+            print("Stopping")
+            print("")
+            break
+
+        try:
+            for token in model.generate(
+                question,
+                max_tokens=4096,
+                streaming=True,
+                temp = 0.7,
+                top_k = 40,
+                top_p = 0.4,
+                repeat_penalty = 1.18,
+                repeat_last_n = 64,
+            ):
+                sys.stdout.write(token)
+                sys.stdout.flush()
+        except KeyboardInterrupt:
+            print("")
+            print("Interrupted")
+            continue
+        finally:
+            print("")
+
+    filename = "chat_" + str(int(time.time()))
+    ntry = 0
+    while ntry < 5:
+        data = model.generate(FILENAME_GENERATION_PROMPT, max_tokens=64)
+        try:
+            print(data)
+            data = json.loads(data)
+            filename = data["conversation_filename"]
+            break
+        except:
+            print("Invalid JSON generated")
+        ntry += 1
+
+    print(f"Saving under filename {filename}")
+    with open(os.path.join(CHAT_DIR, filename), "w") as f:
+        for line in model.current_chat_session:
+            if line["role"] == "user":
+                f.write("USER> " + line["content"] + "\n\n")
+            elif line["role"] == "assistant":
+                f.write("AI> " + line["content"] + "\n\n")
+
+sys.exit(0)
--- flake.nix
+++ flake.nix
@@ -1,12 +1,18 @@
 {
   inputs = {
+    fork.url = "github:litchipi/nixpkgs/gpt4all-bindings";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = inputs: inputs.flake-utils.lib.eachDefaultSystem (system: let
+    forkpkgs = import inputs.fork { inherit system; };
     pkgs = import inputs.nixpkgs {
       inherit system;
+      overlays = [(self: super: {
+        python312 = forkpkgs.python312;
+        python312Packages = forkpkgs.python312Packages;
+      })];
       config = { allowUnfree = true; };
     };
 
@@ -16,81 +22,22 @@
       gpt4all-bindings
     ]);
 
-    gpt4all-backend = pkgs.stdenv.mkDerivation {
-      pname = "gpt4all-backend";
-      inherit (pkgs.gpt4all) src version cmakeFlags nativeBuildInputs buildInputs;
-
-      sourceRoot = "${pkgs.gpt4all.src.name}/gpt4all-backend";
-      installPhase = ''
-        cp -r ./ $out
-      '';
-    };
-
-    gpt4all-bindings = pythonpkg.pkgs.buildPythonPackage {
-      pname = "gpt4all-bindings";
-      inherit (pkgs.gpt4all) src version;
-      sourceRoot = "${pkgs.gpt4all.src.name}/gpt4all-bindings/python";
-      pyproject = false;
-
-      patches = [ ./setup_fixup.patch ];
-
-      propagatedBuildInputs = with pythonpkg.pkgs; [
-        requests
-        tqdm
-      ];
-
-      build-system = with pythonpkg.pkgs; [
-        setuptools
-        wheel
-      ];
-      
-      postPatch = ''
-        substituteInPlace setup.py \
-          --replace-fail 'SRC_CLIB_DIRECTORY = ' 'SRC_CLIB_DIRECTORY = "${gpt4all-backend}" #' \
-          --replace-fail 'SRC_CLIB_BUILD_DIRECTORY = ' 'SRC_CLIB_BUILD_DIRECTORY = "${gpt4all-backend}/" #' \
-          --replace-fail 'DEST_CLIB_BUILD_DIRECTORY = ' 'DEST_CLIB_BUILD_DIRECTORY = os.path.join(DEST_CLIB_DIRECTORY, "build") #'
-      '';
-
-      buildPhase = ''
-        python3 setup.py build
-      '';
-
-      installPhase = ''
-        mkdir -p $out/${pythonpkg.sitePackages}/
-        cp -r gpt4all $out/${pythonpkg.sitePackages}/gpt4all
-      '';
-
-      pythonImportsCheck = ["gpt4all"];
-    };
-
     name = "my_script";
     deps = [
       pythonpkg
-      pkgs.git-lfs
-      gpt4all-bindings
     ];
 
     start = pkgs.writeShellApplication {
       inherit name;
       runtimeInputs = deps;
       text = ''
-        if ! [ -d .model ]; then
-          echo "[*] Getting model"
-          git clone https://huggingface.co/NousResearch/Nous-Hermes-2-Mistral-7B-DPO .model
-          cd .model
-          git lfs install
-          git lfs pull
-          cd ..
-        fi
-        export PYTHONPATH="${pythonpkg}/${pythonpkg.sitePackages}:$PYTHONPATH";
+        export PYTHONPATH="${pythonpkg}/${pythonpkg.sitePackages}";
         export LD_LIBRARY_PATH=${pkgs.gpt4all}/lib:$LD_LIBRARY_PATH
         ${pythonpkg}/bin/python -W ignore ./chat.py
       '';
     };
   in {
     packages.default = start;
-    packages.gpt4all = gpt4all-bindings;
-    packages.gpt4all-backend = gpt4all-backend;
     apps = {
       default = {
         type = "app";

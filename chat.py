62871da13fbf77888cc4c1aaa3638c8c6eb806f8
Litchi Pi <litchi.pi@proton.me>
2024-09-09 16:44:36 +0200

    Improve greatly the chatbot
    Signed-off-by: Litchi Pi <litchi.pi@proton.me>

--- chat.py
+++ chat.py
@@ -1,4 +1,5 @@
 import sys
+import json
 import time
 import os
 from gpt4all import GPT4All
@@ -6,17 +7,34 @@ from gpt4all import GPT4All
 DEFAULT_MODEL_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), ".model")
 DEFAULT_CHAT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), ".chat")
 
+SUPPORTED_MODELS = {
+    "hermes": "Nous-Hermes-2-Mistral-7B-DPO.Q4_0.gguf",
+    "llama": "Meta-Llama-3-8B-Instruct.Q4_0.gguf",
+}
+
+def disp_color(r, g, b, *msg, **kwargs):
+    print(color(r, g, b), end="")
+    print(*msg, **kwargs)
+    print("\033[0m", end="")
+
+def color(r, g, b):
+    return f"\033[38;2;{r};{g};{b}m"
+
+def reset():
+    return "\033[0m"
+
 class Chat(GPT4All):
-    FILENAME_GENERATION_PROMPT = "Create a very short filename for this conversation, output in JSON using the key \"conversation_filename\", only output JSON data."
+    FILENAME_GENERATION_PROMPT = "Create a short filename for this conversation, using key words representing what the user was asking. Output the filename in valid JSON using the key \"conversation_filename\", only output JSON data."
 
     SYSTEM_PROMPT = """
     """
-    def __init__(self, model_dir=DEFAULT_MODEL_DIR, chat_dir=DEFAULT_CHAT_DIR):
+    def __init__(self, model, model_dir=DEFAULT_MODEL_DIR, chat_dir=DEFAULT_CHAT_DIR):
+        self.msg_system("Loading model", model.capitalize())
         os.makedirs(model_dir, exist_ok=True)
         self.model_dir = model_dir
 
         GPT4All.__init__(self,
-            model_name = "Meta-Llama-3-8B-Instruct.Q4_0.gguf",
+            model_name = SUPPORTED_MODELS[model],
             model_path = self.model_dir,
             allow_download = True,
             n_threads = 16,
@@ -24,97 +42,102 @@ class Chat(GPT4All):
 
         os.makedirs(chat_dir, exist_ok=True)
         self.chat_dir = chat_dir
+        self._history = [{"role": "system", "content": self.SYSTEM_PROMPT}]
+        self._current_prompt_template = self.config["promptTemplate"]
 
-    # TODO    Break this down into a much more modular code using methods & stuff
-    def start_session(self):
-        with self.chat_session():
-            while True:
-                try:
-                    question = input("> ")
-                except:
-                    print("Stopping")
-                    print("")
-                    break
+    def msg_system(self, *msg):
+        disp_color(128, 128, 128, *msg)
 
-                if question == "/quit":
-                    print("Stopping")
-                    print("")
-                    break
+    def msg_ai(self, *msg):
+        disp_color(255, 128, 200, *msg)
 
-                tokens = self.generate(
-                        question,
-                        max_tokens=4096,
-                        streaming=True,
-                        temp = 0.7,
-                        top_k = 40,
-                        top_p = 0.4,
-                        repeat_penalty = 1.18,
-                        repeat_last_n = 64,
-                    )
-                while True:
-                    try:
-                        token = next(tokens)
-                    except StopIteration:
+    def token_callback(self, id, token):
+        try:
+            self.response_buffer.append(token)
+
+            if self.disp_tokens:
+                sys.stdout.write(token)
+                sys.stdout.flush()
+        except KeyboardInterrupt:
+            self.msg_system("Interrupted")
+            return False
+
+        return self.continue_token_generation
+    
+    def ask(self, question, **kwargs):
+        config = dict(
+            max_tokens=4096,
+            temp = 0.7,
+            top_k = 40,
+            top_p = 0.4,
+            repeat_penalty = 1.18,
+            repeat_last_n = 64,
+        )
+        config.update(kwargs)
+        config["streaming"] = False
+
+        self.response_buffer = []
+        self.disp_tokens=True
+        self.continue_token_generation = True
+        try:
+            print(color(255, 128, 200), end="")
+            response = self.generate(question, callback=self.token_callback, **config)
+        except KeyboardInterrupt:
+            self.msg_system("Interrupt\n")
+        finally:
+            print(reset(), end="")
+            self.continue_token_generation = False
+        print("")
+        response = ''.join(self.response_buffer)
+        return response
+
+    def run(self):
+        while True:
+            try:
+                question = input(color(128, 255, 200) + "> ")
+                print(reset(), end="")
+            except KeyboardInterrupt:
+                self.msg_system("Stopping\n")
+                break
+
+            if question == "/quit":
+                self.msg_system("Stopping\n")
+                break
+
+            self.ask(question)
+
+        self.msg_system("Generating a filename for this conversation")
+        filename = self.generate_filename()
+        self.msg_system(f"Saving under filename {filename}")
+        with open(os.path.join(self.chat_dir, filename), "w") as f:
+            for line in self.current_chat_session:
+                if line["role"] == "user":
+                    if line["content"] == self.FILENAME_GENERATION_PROMPT:
                         break
-                    # FIXME     Segmentation fault if try to use prompt afterward
-                    except KeyboardInterrupt:
-                        del tokens
-                        print("Interrupted")
-                        break
-                    sys.stdout.write(token)
-                    sys.stdout.flush()
+                    f.write("USER> " + line["content"] + "\n\n")
+                elif line["role"] == "assistant":
+                    f.write("AI> " + line["content"] + "\n\n")
 
-            filename = "chat_" + str(int(time.time()))
-            ntry = 0
-            while ntry < 5:
-                data = self.generate(FILENAME_GENERATION_PROMPT, max_tokens=64)
-                try:
-                    print("text", data)
-                    data = json.loads(data)
-                    print("json", data)
-                    filename = data["conversation_filename"]
-                    print("filename", filename)
-                    break
-                except Exception as err:
-                    print(f"Invalid JSON generated: {err}")
-                ntry += 1
+    def generate_filename(self):
+        self.disp_tokens = False
+        filename = "chat_" + str(int(time.time()))
+        ntry = 0
+        while ntry < 5:
+            try:
+                data = self.generate(self.FILENAME_GENERATION_PROMPT, max_tokens=64)
+            except KeyboardInterrupt:
+                break
 
-            print(f"Saving under filename {filename}")
-            with open(os.path.join(CHAT_DIR, filename), "w") as f:
-                for line in self.current_chat_session:
-                    if line["role"] == "user":
-                        if line["content"] == FILENAME_GENERATION_PROMPT:
-                            break
-                        f.write("USER> " + line["content"] + "\n\n")
-                    elif line["role"] == "assistant":
-                        f.write("AI> " + line["content"] + "\n\n")
+            try:
+                data = json.loads(data)
+                filename = data["conversation_filename"]
+            except KeyboardInterrupt:
+                break
+            except Exception as err:
+                self.msg_system(f"Invalid JSON generated: {err}")
 
-        sys.exit(0)
+            ntry += 1
+        return filename
 
-# TODO    Use this code to stream elements and stop iteration when we want
-
-from gpt4all import GPT4All
-import sys
-
-model = GPT4All('ggml-mpt-7b-chat')
-message = sys.sysv[1]
-messages = []
-print( "Prompt: " + message )
-messages.append({"role": "user", "content": message});
-full_prompt = model._build_prompt(messages, True, True)
-response_tokens = [];
-def local_callback(token_id, response):
-    decoded_token = response.decode('utf-8')
-    response_tokens.append( decoded_token );
-
-    # Do whatever you want with decoded_token here.
-
-    return True
-
-model.model._response_callback = local_callback
-model.model.generate(full_prompt, streaming=False)
-response = ''.join(response_tokens)
-print ( "Response: " + response );
-messages.append({"role": "assistant", "content": response});
-
-# At this point, you can get another prompt from the user, re-run "_build_prompt()", and continue the conversation.
+bot = Chat("hermes")
+bot.run()
